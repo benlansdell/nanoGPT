f673d8b54ee8cb47caa0959519a5dad72a508fb4
benlansdell <ben.lansdell@stjude.org>
2023-03-24 15:50:17 +0000

    Setup optuna search

---
+++ config/train_gpt2_wsi_optuna.py
@@ -0,0 +1,54 @@
+# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
+# launch as the following (e.g. in a screen session) and wait ~5 days:
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+
+wandb_log = False
+wandb_project = 'hande'
+wandb_run_name = 'mini-gpt'
+
+#simple parameters
+optuna_search
+block_size
+n_layer
+dropout
+weight decay
+learning rate
+n_head
+
+#Params that require changes to model
+architecture
+number of linear layers at end
+flatten, or take mean over middle layer to get pre-activation layer
+
+# these make the total batch size be ~0.5M
+# 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
+batch_size = 32 #Number of slides to randomly choose
+block_size = 128 #Number of patches from each slide to randomly choose
+gradient_accumulation_steps = 5
+
+#Number of disease types we're classifying
+n_classes = 17
+
+#Baby GPT
+n_layer = 6
+n_head = 8
+n_embd = 512
+dropout = 0.2
+
+learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+max_iters = 5000
+lr_decay_iters = 5000 # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 100 # not super necessary potentially
+
+dataset = 'simclr-ciga512_10'
+
+#Eval stuff
+eval_interval = 250 # keep frequent because we'll overfit
+eval_iters = 200
+log_interval = 10 # don't print too too often
+
+# weight decay
+weight_decay = 1e-1
